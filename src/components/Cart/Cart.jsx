1197e5b0a6944398d0f80542ee84fa6465ce5c0e
anUnnecessary <raihanshamil33@gmail.com>
2023-05-15 11:38:41 +0600

    first commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import './Cart.css'
+
+const Cart = ({cart, handleClearCart, children}) => {
+    // const cart = props.cart; option 1
+    // const {cart} = props; //option 2
+
+    // console.log(cart);
+
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+    }
+
+    const tax = totalPrice*7/100;
+
+    const grandTotal = totalPrice + totalShipping + tax;
+
+    return (
+        <div className='cart'>
+            <h4>Order Summary:</h4>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)} </h4>
+            <button onClick={handleClearCart} className='btn-clear-cart'><span>Clear Cart</span>  <FontAwesomeIcon icon= {faTrashAlt} /></button>
+            {children}
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import './ReviewItem.css';
+
+const ReviewItem = ({ product, handleRemoveFromCart }) => {
+    const { _id, img, name, price, quantity, } = product;
+    return (
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='orange-txt'>${price}</span></p>
+                <p>Order Quantity: <span className='orange-txt'>{quantity}</span></p>
+                <button onClick={() => handleRemoveFromCart(_id)} className='btn-delete'><FontAwesomeIcon className='delete-icon' icon= {faTrashAlt} /> </button>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewItem;
